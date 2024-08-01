57395f737e62afa3df2b91ff1296f6f0018cc315
f4deb <f4deb1@gmail.com>
2024-08-01 22:42:02 +0200

    VSCODE init config OK

---
+++ VHF-144-NANO/SSB_VHF_F4DEB/build/libraries/Wire/utility/twi.c.d
@@ -0,0 +1,7 @@
+/home/f4deb/Documents/ESP32/VHF-144-NANO/SSB_VHF_F4DEB/build/libraries/Wire/utility/twi.c.o: \
+ /home/f4deb/.arduino15/packages/arduino/hardware/avr/1.8.6/libraries/Wire/src/utility/twi.c \
+ /home/f4deb/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Arduino.h \
+ /home/f4deb/.arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/binary.h \
+ /home/f4deb/.arduino15/packages/arduino/hardware/avr/1.8.6/variants/eightanaloginputs/pins_arduino.h \
+ /home/f4deb/.arduino15/packages/arduino/hardware/avr/1.8.6/variants/eightanaloginputs/../standard/pins_arduino.h \
+ /home/f4deb/.arduino15/packages/arduino/hardware/avr/1.8.6/libraries/Wire/src/utility/twi.h
